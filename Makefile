18dcafb59aaabea7f870367d92265bf2fff91922
Doug Davis <ddavis@ddavis.io>
2019-10-21 14:48:19 -0400

    more free functions; add recursive iterator

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 CXX=/home/ddavis/Software/gcc/9.2.0/bin/c++
 
 all:
-	$(CXX) -static-libstdc++ -O3 -Wall -shared -std=c++17 -fPIC `python3 -m pybind11 --includes` stdfs.cpp -o stdfs`python3-config --extension-suffix`; strip stdfs*so
+	$(CXX) -static-libstdc++ -march=x86-64 -m64 -O3 -Wall -shared -std=c++17 -fPIC `python3 -m pybind11 --includes` stdfs.cpp -o stdfs`python3-config --extension-suffix`; strip stdfs*so
--- stdfs.cpp
+++ stdfs.cpp
@@ -12,7 +12,7 @@ namespace fs = std::filesystem;
 PYBIND11_MODULE(stdfs, m) {
   m.doc() = "std::filesystem in Python";
 
-  // std::filesystem::path
+  // std::filesystem::path binding
 
   py::class_<fs::path>(m, "path")
     .def(py::init<const std::string&>())
@@ -36,7 +36,7 @@ PYBIND11_MODULE(stdfs, m) {
     .def("root_directory", [](const fs::path& p) { return p.root_directory(); })
     .def("root_path", [](const fs::path& p) { return p.root_path(); });
 
-  // std::filesystem::directory_entry
+  // std::filesystem::directory_entry binding
 
   py::class_<fs::directory_entry>(m, "directory_entry")
     .def(py::init<const fs::path&>())
@@ -53,11 +53,33 @@ PYBIND11_MODULE(stdfs, m) {
     .def("is_block_file", [](const fs::directory_entry& de) { return de.is_block_file(); })
     .def("is_character_file", [](const fs::directory_entry& de) { return de.is_character_file(); });
 
-  // free functions
+  // free function bindings
 
-  m.def("exists", [](const fs::path& p) { return fs::exists(p); });
+  m.def("absolute", [](const fs::path& p) { return fs::absolute(p); });
+  m.def("canonical", [](const fs::path& p) { return fs::canonical(p); });
   m.def("create_directory", [](const fs::path& p) { return fs::create_directory(p); });
   m.def("create_directories", [](const fs::path& p) { return fs::create_directories(p); });
+  m.def("current_path", []() { return fs::current_path(); });
+  m.def("equivalent", [](const fs::path& p1, const fs::path& p2) { return fs::equivalent(p1, p2); });
+  m.def("file_size", [](const fs::path& p) { return fs::file_size(p); });
+  m.def("exists", [](const fs::path& p) { return fs::exists(p); });
+  m.def("proximate", [](const fs::path& p) { return fs::proximate(p); });
+  m.def("relative", [](const fs::path& p) { return fs::relative(p); });
   m.def("temp_directory_path", []() { return fs::temp_directory_path(); });
+  m.def("weakly_canonical", [](const fs::path& p) { return fs::weakly_canonical(p); });
+
+  // iterator bindings
+
+  py::class_<fs::directory_iterator>(m, "directory_iterator")
+    .def(py::init<const fs::path&>())
+    .def("__iter__", [](const fs::directory_iterator& di) {
+      return py::make_iterator(fs::begin(di), fs::end(di));
+    }, py::keep_alive<0, 1>());
+
+  py::class_<fs::recursive_directory_iterator>(m, "recursive_directory_iterator")
+    .def(py::init<const fs::path&>())
+    .def("__iter__", [](const fs::recursive_directory_iterator& rdi) {
+      return py::make_iterator(fs::begin(rdi), fs::end(rdi));
+    }, py::keep_alive<0, 1>());
 
 }
